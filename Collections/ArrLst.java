2988fb0ad67ab44e182a22d7d539fd3714ffe12f
chMahesh <maheshchimuturi143@gmail.com>
2025-01-16 11:27:42 +0530

    second commit

---
+++ Collections/ArrLst.java
@@ -0,0 +1,17 @@
+package Collections;
+
+import java.util.ArrayList;
+
+public class ArrLst {
+
+	public static void main(String[] args) {
+		ArrayList Mahesh = new ArrayList<>();
+		Mahesh.add("My");
+		Mahesh.add("Name");
+		Mahesh.add("Is");
+		Mahesh.add("Mahesh.");
+		System.out.println(Mahesh);
+
+	}
+
+}
